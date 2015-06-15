3e6c07e7dcc382b44c089fcc6c1c764f36b5da57
Xenock <miguelmoracho@gmail.com>
2015-06-15 19:58:40 +0200

    Added named route for new User

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
-  get 'contact' =>'static_pages#contact'
+  get 'contact' => 'static_pages#contact'
+  get 'signup'  => 'users#new'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
