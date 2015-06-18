d874ad4649d22962aeb6abce56b037d1fa158eb2
Xenock <miguelmoracho@gmail.com>
2015-06-18 02:36:02 +0200

    Successful user edit. TDD GREEN

--- test/integration/user_edit_test.rb
+++ test/integration/user_edit_test.rb
@@ -21,12 +21,12 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
     name = "Foo Bar"
     email = "foo@bar.com"
-    patch user_path(@user), user {  name: name,
+    patch user_path(@user), user: {  name: name,
                                     email: email,
                                     password: "",
                                     password_confirmation: ""}
     assert_not flash.empty?
-    assert_redirect_to @user
+    assert_redirected_to @user
     @user.reload
     assert_equal name, @user.name
     assert_equal email, @user.email
