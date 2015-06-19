f512dbdc6d1be965333d786996699e24880cb33d
Xenock <miguelmoracho@gmail.com>
2015-06-19 05:14:24 +0200

    Added test, fixtures and code to implement delete to admin role

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
+User.create!(name:  "miguel",
+             email: "miguelmoracho@gmail.com",
              password:              "foobar",
-             password_confirmation: "foobar")
+             password_confirmation: "foobar",
+             admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
