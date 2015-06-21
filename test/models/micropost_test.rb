3719663aae62c8322c621a2dc4178fa7a4655df1
Xenock <miguelmoracho@gmail.com>
2015-06-21 15:26:32 +0200

    Default scope micropost

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -26,4 +26,7 @@ class MicropostTest < ActiveSupport::TestCase
     assert_not @micropost.valid?
   end
 
+  test "order should be most recent first" do
+    assert_equal microposts(:most_recent), Micropost.first
+  end
 end
